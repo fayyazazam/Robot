5f01fea94466a8d54f21538177dfa00f7941363e
Fayyaz Azam <fayyaz@Fayyazs-MacBook-Air.local>
2014-09-17 19:46:49 -0700

    First commit of BOT-GAME

---
+++ src/com/reindeer/robotgame/SplashLoadingScreen.java
@@ -0,0 +1,46 @@
+package com.reindeer.robotgame;
+
+import com.reindeer.framework.Game;
+import com.reindeer.framework.Graphics;
+import com.reindeer.framework.Screen;
+import com.reindeer.framework.Graphics.ImageFormat;
+
+public class SplashLoadingScreen extends Screen {
+	public SplashLoadingScreen(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		Graphics g = game.getGraphics();
+		Assets.splash= g.newImage("splash.jpg", ImageFormat.RGB565);
+		
+		game.setScreen(new LoadingScreen(game));
+
+	}
+
+	@Override
+	public void paint(float deltaTime) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void backButton() {
+
+	}
+}
